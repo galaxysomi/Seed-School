9869c9c2864b4fc452a7d93d19f6a5f8c8d50065
trungtrung <taductrung1709@gmail.com>
2022-01-17 14:56:11 +0700

    sua hoat dong admin

---
+++ template/pagesAdmin/activity/activityManagement.js
@@ -0,0 +1,25 @@
+function findSoTheoDoi() {
+    axios.get("http://localhost:3000/api/admin/activities")  // dien link api vao
+      .then((activities) => { 
+        console.log(activities.data);           
+        let info = " ";
+        $.each(activities.data, function (index, value) {
+          info += `
+          <tr>
+            <td> ${value.date}  </td>
+            <td> ${value.timeStart} - ${value.timeFinish} </td>
+            <td> ${value.title}</td>                        
+            <td> ${value.place}</td>
+            
+          </tr>            
+           `            
+        });         
+        $('#information').html(info);
+      });
+    }
+            
+  function submitSuccess(){
+    alert("Submit thành công");
+}      
+
+$( document ).ready(findSoTheoDoi());
