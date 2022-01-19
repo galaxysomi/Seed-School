32504272fb9558fbbf593b39789710e064b725d0
trungtrung <taductrung1709@gmail.com>
2022-01-19 19:19:30 +0700

    trung

--- template/PageTeacher/QLThoiKhoaBieu/QLthoikhoabieu.js
+++ template/PageTeacher/QLThoiKhoaBieu/QLthoikhoabieu.js
@@ -4,25 +4,23 @@ axios.get(host + '/api/teacher/schedule', {
   headers: { Authorization: 'Bearer ' + localStorage.token }
 }).then(result => {
   if (result.data.status === 'ok') {
-    // console.log(result.data.schedules[0]);
-    document.getElementById("invisibleID").value = result.data.schedules[0]._id ;
-    console.log(document.getElementById("invisibleID").value);
-    let schedules = result.data.schedules[0].activityList;
+     console.log(result.data.schedules[0]);
+    document.getElementById("invisibleID").value = result.data.schedules[0]._id;
+    // console.log(document.getElementById("invisibleID").value);
+    let activities = result.data.schedules[0].activityList;
     let info = " ";
-    $.each(schedules, function (index, value) {
+    $.each(activities, function (index, value) {
       info += `
           <tr>            
-          <td> ${new Date(value.start).getDate()}/${new Date(value.start).getMonth()+1}/${new Date(value.start).getFullYear()} </td>
+          <td> ${new Date(value.start).getDate()}/${new Date(value.start).getMonth() + 1}/${new Date(value.start).getFullYear()} </td>
           <td> ${new Date(value.start).getHours()}:${new Date(value.start).getMinutes()}-${new Date(value.end).getHours()}:${new Date(value.end).getMinutes()}</td>            
           <td> ${value.content} </td>
           <td>
-          <button    type="button" onclick= deleteMenu() class="btn btn-danger" data-toggle="modal" data-target="#deleteActivity">
+          <button type="button" onclick="deleteActivity('${result.data.schedules[0]._id}','${value._id}')" class="btn btn-danger" >
             Xóa
           </button>
-          </td>
-                             
+          </td>                             
         </tr>            
-           
              `
 
     });
@@ -38,42 +36,78 @@ function addActivity() {
   console.log(document.getElementById("start").value);
   console.log(document.getElementById("end").value);
   console.log(document.getElementById("date").value);
-  
 
 
-  axios.post('http://localhost:3000/api/teacher/activity',{
-    scheduleId : document.getElementById("invisibleID").value,
-    date : document.getElementById("date").value,
-    newActivity : {
-      start :document.getElementById("start").value,
-      end :document.getElementById("end").value,
-      content :document.getElementById("content").value
+
+  axios.post('http://localhost:3000/api/teacher/activity', {
+    scheduleId: document.getElementById("invisibleID").value,
+    date: document.getElementById("date").value,
+    newActivity: {
+      start: document.getElementById("start").value,
+      end: document.getElementById("end").value,
+      content: document.getElementById("content").value
     }
   }, {
     headers: { Authorization: 'Bearer ' + localStorage.token }
-})
-  .then((rs) => {
-    console.log(rs);
-    if(rs.data.success){
-      alert(rs.data.message);
-    }else{
-      alert(rs.data.message);
-    }
-    window.location.reload();             
-  })   
+  })
+    .then((rs) => {
+      console.log(rs);
+      if (rs.data.success) {
+        alert(rs.data.message);
+      } else {
+        alert(rs.data.message);
+      }
+      window.location.reload();
+    })
 }
 
 
-function deleteMenu() {
-  const id = document.getElementById("invisibleID").value
-  axios.delete('http://localhost:3000/api/teacher/activity/'+ id,{
+function deleteActivity(scheduleId , activityId) {
+  axios.delete('http://localhost:3000/api/teacher/activity',{
+    scheduleId : scheduleId ,
+    activityId : activityId
+  }, {
     headers: { Authorization: 'Bearer ' + localStorage.token }
-})
-  .then((rs) => {
-    console.log(rs);
-    if(rs.data.status== "ok"){
-    alert("Xóa menu thành công")
-    refreshPage();
-  }
+  })
+    .then((rs) => {
+      console.log(rs);
+      if (rs.data.status == "ok") {
+        alert("Xóa thành công")
+        window.location.reload();
+
+      }
+    })
+}
+
+
+function findActivityByDate() {
+  const date = document.getElementById("date").value ;
+  const host = 'http://localhost:3000'
+  axios.get(host + '/api/teacher/schedule/?date='+date, {
+    headers: { Authorization: 'Bearer ' + localStorage.token }
+  }).then(result => {
+    if (result.data.status === 'ok') {
+      console.log(result);
+      // console.log(result.data.schedules[0]);
+      document.getElementById("invisibleID").value = result.data.schedules[0]._id;
+      // console.log(document.getElementById("invisibleID").value);
+      let schedules = result.data.schedules[0].activityList;
+      let info = " ";
+      $.each(schedules, function (index, value) {
+        info += `
+          <tr>            
+          <td> ${new Date(value.start).getDate()}/${new Date(value.start).getMonth() + 1}/${new Date(value.start).getFullYear()} </td>
+          <td> ${new Date(value.start).getHours()}:${new Date(value.start).getMinutes()}-${new Date(value.end).getHours()}:${new Date(value.end).getMinutes()}</td>            
+          <td> ${value.content} </td>
+          <td>
+          <button    type="button" onclick= deleteMenu() class="btn btn-danger" data-toggle="modal" data-target="#deleteActivity">
+            Xóa
+          </button>
+          </td>                          
+        </tr>                     
+             `
+      });
+      $('#information').html(info);
+    }
   })
 }
